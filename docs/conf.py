29c706f45b4ab7f80d51156b445d866805081653
Nozomu Kaneko <nozom.kaneko@gmail.com>
2012-02-26 15:06:23 +0900

    Add '(translation)' suffix to html_title.

--- docs/conf.py
+++ docs/conf.py
@@ -200,7 +200,7 @@ html_theme = 'doc-ja'
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
-html_title = 'The Pyramid Web Application Development Framework v%s' % release
+html_title = u'The Pyramid Web Application Development Framework v%s (翻訳)' % release
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
 #html_short_title = 'Home'
